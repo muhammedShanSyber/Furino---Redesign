eaf6dc97bd1abbeed23e074188726782070eaeb9
Mohammed Shan N <suharabeevi443@outlook.com>
2024-11-23 15:09:55 +0530

    fixed text issues

--- src/App.jsx
+++ src/App.jsx
@@ -15,15 +15,12 @@ function App() {
     <>
       <nav className="bg-white  fixed w-full z-20 top-0 start-0 border-b border-gray-200 ">
         <div className=" flex flex-wrap items-center justify-between mx-auto p-4">
-          <a
-            href="https://flowbite.com/"
-            className="flex items-center space-x-3 rtl:space-x-reverse"
-          >
+          <div className="flex items-center space-x-3 rtl:space-x-reverse">
             <img src={"public/logo.png"} className="h-8" alt="Flowbite Logo" />
             <span className="self-center text-4xl font-bold whitespace-nowrap text-black">
               Furniro
             </span>
-          </a>
+          </div>
           <div className="flex md:order-2  w-fit">
             <BsPersonExclamation className="w-8 h-8 font-bold mr-5" />
             <IoSearch className="w-8 h-8 font-bold mr-5" />
@@ -83,7 +80,76 @@ function App() {
         {/* Logo and Text - Centered */}
         <div className="absolute inset-0 flex flex-col items-center justify-center">
           <img src="public/logo.png" className="h-8 mb-2" alt="Flowbite Logo" />
-          <span className="text-lg font-semibold text-black">Cart</span>
+          <span className="text-4xl font-semibold text-black">Cart</span>
+
+          <nav className="flex" aria-label="Breadcrumb">
+            <ol className="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
+              <li className="inline-flex items-center">
+                <a
+                  href="#"
+                  className="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white"
+                >
+                  <svg
+                    className="w-3 h-3 me-2.5"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                  >
+                    <path d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z" />
+                  </svg>
+                  Home
+                </a>
+              </li>
+              <li>
+                <div className="flex items-center">
+                  <svg
+                    className="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 6 10"
+                  >
+                    <path
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="m1 9 4-4-4-4"
+                    />
+                  </svg>
+                  <a
+                    href="#"
+                    className="ms-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ms-2 dark:text-gray-400 dark:hover:text-white"
+                  >
+                    Projects
+                  </a>
+                </div>
+              </li>
+              <li aria-current="page">
+                <div className="flex items-center">
+                  <svg
+                    className="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 6 10"
+                  >
+                    <path
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="m1 9 4-4-4-4"
+                    />
+                  </svg>
+                  <span className="ms-1 text-sm font-medium text-gray-500 md:ms-2 dark:text-gray-400">
+                    Flowbite
+                  </span>
+                </div>
+              </li>
+            </ol>
+          </nav>
         </div>
       </div>
 
